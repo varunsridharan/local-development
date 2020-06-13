674e494149bfbd707dbc8b498564dd0ded7d7531
Varun <varunsridharan23@gmail.com>
2020-06-13 15:02:17 +0530

    Updated

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
-define( 'VSP_LOCAL_DIR', __DIR__ . '/' );
+require_once __DIR__ . 'global/functions.php';
 
-require_once VSP_LOCAL_DIR . 'global/functions.php';
+vsp_local_define( 'VSP_LOCAL_DIR', __DIR__ . '/' );
+
+vsp_local_define( 'VSP_WP_LOCAL_TEMPLATE', 'E:\localhost\www\wp\template\\' );
+
+vsp_local_define( 'LOCAL_SMTP_HOST', '127.0.0.1' );
+
+vsp_local_define( 'LOCAL_SMTP_PORT', '2525' );
+
+vsp_local_define( 'LOCAL_SMTP_AUTH', false );
+
 
 // Checks if its a log view
 is_debug_view();
