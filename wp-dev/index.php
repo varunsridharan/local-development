ba416511f388897546e4d3ad8a97b5c21219a40b
Varun <varunsridharan23@gmail.com>
2020-06-13 16:01:40 +0530

    Added Debug Log handler.

--- wp-dev/index.php
+++ wp-dev/index.php
@@ -47,6 +47,8 @@ if ( ! class_exists( 'VSP_Local_WP_Handler' ) ) {
 				VSP_LOCAL_DIR . 'wp-plugins/inspector'            => 'wp-content/mu-plugins/inspector/inspector.php',
 				'debug-quick-look'                                => 'wp-content/mu-plugins/debug-quick-look/debug-quick-look.php',
 			);
+
+			$this->handle_debug_log_file();
 		}
 
 		/**
@@ -56,23 +58,37 @@ if ( ! class_exists( 'VSP_Local_WP_Handler' ) ) {
 		 */
 		public function setup_smtp_info( $phpmailer ) {
 			if ( defined( 'LOCAL_SMTP_HOST' ) ) {
-				$phpmailer->Host = LOCAL_SMTP_HOST;
+				$phpmailer->Host = LOCAL_SMTP_HOST; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			}
 			if ( defined( 'LOCAL_SMTP_PORT' ) ) {
-				$phpmailer->Port = LOCAL_SMTP_PORT;
+				$phpmailer->Port = LOCAL_SMTP_PORT; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			}
 			if ( defined( 'LOCAL_SMTP_AUTH' ) ) {
-				$phpmailer->SMTPAuth = LOCAL_SMTP_AUTH;
+				$phpmailer->SMTPAuth = LOCAL_SMTP_AUTH; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			}
 			if ( defined( 'LOCAL_SMTP_UERNAME' ) ) {
-				$phpmailer->Username = LOCAL_SMTP_UERNAME;
+				$phpmailer->Username = LOCAL_SMTP_UERNAME; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			}
 			if ( defined( 'LOCAL_SMTP_PASSWORD' ) ) {
-				$phpmailer->Password = LOCAL_SMTP_PASSWORD;
+				$phpmailer->Password = LOCAL_SMTP_PASSWORD; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 			}
 
 			if ( defined( 'LOCAL_SMTP_HOST' ) && defined( 'LOCAL_SMTP_PORT' ) ) {
 				$phpmailer->isSMTP();
+			}
+		}
+
+		/**
+		 * Handles Debug Log File.
+		 */
+		protected function handle_debug_log_file() {
+			$ctime    = date( 'D-d-M-Y-h-i-s-a' );
+			$old_file = ABSPATH . 'wp-content/debug.log';
+			$size     = ( file_exists( $old_file ) ) ? filesize( $old_file ) : 0;
+			if ( $size >= WP_DEBUG_MAX_SIZE ) {
+				@mkdir( ABSPATH . 'wp-content/' . WP_DEBUG_LOG_OVERFLOW_STORAGE . '/' );
+				copy( $old_file, ABSPATH . 'wp-content/' . WP_DEBUG_LOG_OVERFLOW_STORAGE . '/' . $ctime . '.log' );
+				@file_put_contents( ABSPATH . 'wp-content/debug.log', '' );
 			}
 		}
 	}
