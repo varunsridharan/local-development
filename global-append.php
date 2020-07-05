ea53904c6b25aec0ac19f35d3cda03817e1abcb6
Varun <varunsridharan23@gmail.com>
2020-07-05 10:41:17 +0530

    Fixed Some ErROR

--- global-append.php
+++ global-append.php
@@ -1,9 +1,13 @@
 <?php
 
 $time_took = vsp_timer( 'vsp-local-process-start', true, 10 );
-echo <<<HTML
+/**
+ * <script>
+ * var vsp_local_time_took = $time_took;
+ * </script>
+ */
+if ( ! in_array( 'content-type: application/json', headers_list() ) ) {
+	echo <<<HTML
 <!-- Total Time Took For PHP Process : $time_took -->
-<script>
-var vsp_local_time_took = $time_took;
-</script>
 HTML;
+}
